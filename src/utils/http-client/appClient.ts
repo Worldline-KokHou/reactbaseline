45cbc199551b73f0a213aafb484210b749b72b91
Kevin Lu <kevin.lu@worldline.com>
2021-09-15 17:33:10 +0800

    add appClient and utils index

---
+++ src/utils/http-client/appClient.ts
@@ -0,0 +1,16 @@
+import Axios from 'axios'
+import ENVIRONMENT from '../constants/environment.constant'
+
+const appClient = Axios.create({
+  baseURL: ENVIRONMENT.baseUrl(),
+  timeout: 60 * 1000, // 60 sec timeout
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+if (!ENVIRONMENT.isProduction()) {
+  console.log('AppClient Configuration: ', appClient.defaults)
+}
+
+export { appClient }
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+import ENVIRONMENT from './constants/environment.constant'
+import { appClient } from './http-client/appClient'
+
+export {
+  appClient,
+  ENVIRONMENT
+}
