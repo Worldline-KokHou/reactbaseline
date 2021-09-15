ed751187b907307073a77c1af2fcf049c4303b19
Kevin Lu <kevin.lu@worldline.com>
2021-09-15 18:11:29 +0800

    add interceptor for appClient

--- src/utils/http-client/appClient.ts
+++ src/utils/http-client/appClient.ts
@@ -3,7 +3,7 @@ import { ENVIRONMENT } from '@constants'
 
 const appClient = Axios.create({
   baseURL: ENVIRONMENT.baseUrl(),
-  timeout: 60 * 1000, // 60 sec timeout
+  timeout: 1, // 60 sec timeout
   headers: {
     'Content-Type': 'application/json'
   }
@@ -13,4 +13,29 @@ if (!ENVIRONMENT.isProduction()) {
   console.log('AppClient Configuration: ', appClient.defaults)
 }
 
+appClient.interceptors.response.use(
+  (value) => {
+    if (!ENVIRONMENT.isProduction()) {
+      console.log('AppClient interceptors.response: ', value)
+    }
+    return value
+  },
+  (error) => {
+    if (!ENVIRONMENT.isProduction()) {
+      console.group('interceptors.response.onFail')
+      console.warn(error)
+      console.warn('JSON', error.toJSON())
+      console.groupEnd()
+    }
+
+    return error
+  }
+)
+
+appClient.interceptors.request.use((value) => {
+  if (!ENVIRONMENT.isProduction()) {
+    console.log('AppClient interceptors.request: ', value)
+  }
+  return value
+})
 export default appClient
